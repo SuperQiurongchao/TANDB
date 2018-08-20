4a8f4ca9e8d66f84dc9e088cb2e244578f4dae7f
SuperQiurongchao <qiurongchao@p1.com>
2018-08-20 17:27:16 +0800

    Initial Commit

---
+++ TANDB/TANDB.h
@@ -0,0 +1,19 @@
+//
+//  TANDB.h
+//  TANDB
+//
+//  Created by 邱荣超 on 2018/8/20.
+//  Copyright © 2018年 邱荣超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TANDB.
+FOUNDATION_EXPORT double TANDBVersionNumber;
+
+//! Project version string for TANDB.
+FOUNDATION_EXPORT const unsigned char TANDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TANDB/PublicHeader.h>
+
+
